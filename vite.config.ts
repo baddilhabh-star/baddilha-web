32c70953b88b42922e2e429d34acd2a068fddbba
baddilhabh-star <baddilhabh@gmail.com>
2026-01-08 03:42:08 +0300

    fix base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,8 @@ export default defineConfig(({ mode }) => ({
     react(),
     ...(mode === "development" ? [componentTagger()] : []),
   ],
-  base: mode === "development" ? "/" : "/baddilha-web/",
+  base: "/baddilha-web/",
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
